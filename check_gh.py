6a5db37d2507eae769bdfc4a886da7adbf94cf87
k27dong <me@kefan.me>
2023-05-29 18:30:19 -0400

    temp changes for testing

--- check_gh.py
+++ check_gh.py
@@ -1,12 +1,15 @@
 import sys
 from github import Github
-from datetime import datetime, timezone
+from datetime import datetime, timedelta
 
 pat = sys.argv[1]
 latest_commit_time: datetime = None
 
 for repo in Github(pat).get_user().get_repos():
     try:
+        # For now there's an issue with the API (not sure if it's by GitHub or PyGitHub), where the
+        # timezone it returned is correct, but the time itself is actually set in UTC.
+        # On the current implementation this won't affect anything, however it's something to keep in mind.
         curr_last_commit_time = repo.get_commits()[0].commit.committer.date
         if latest_commit_time is None or curr_last_commit_time > latest_commit_time:
             latest_commit_time = curr_last_commit_time
@@ -15,27 +18,14 @@ for repo in Github(pat).get_user().get_repos():
         # it won't be accessible but still shows up in the API
         continue
 
-# For now there's an issue with the API (not sure if it's by GitHub or PyGitHub), where the
-# timezone it returned is correct, but the time is actually in UTC. So we need to correct it manually.
-# Hopefully this will be fixed soon.
-
-# get the correct timezone returned from the API
-# target_timezone = latest_commit_time.astimezone().tzinfo
-
-# convert the time to UTC, then convert it to the correct timezone, then remove the offset stamp
-# latest_commit_time = (
-#     latest_commit_time.replace(tzinfo=timezone.utc)
-#     .astimezone(tz=target_timezone)
-#     .replace(tzinfo=None)
-# )
-
-# Check if there's a push today after 3am
+# This script would run everyday at 8pm, to avoid timezone issues we'll just check if the last commit
+# is made within 17 hours of the current time. (3AM)
 today = datetime.now()
-today = today.replace(hour=3, minute=0, second=0, microsecond=0)
 
-print(f"Today: {today}")
+print(f"Now: {today}")
+print(f"diff: {today - latest_commit_time}")
 
-if latest_commit_time is None or latest_commit_time < today:
+if latest_commit_time is None or today - latest_commit_time > timedelta(hours=17):
     print("No commits found.")
     sys.exit(1)
 
