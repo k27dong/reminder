2097af7d5e85e7e45a859ac3d6baf8e2052bc273
k27dong <me@kefan.me>
2023-05-30 13:16:46 -0400

    temp changes for testing

--- check_gh.py
+++ check_gh.py
@@ -22,6 +22,8 @@ for repo in Github(pat).get_user().get_repos():
 # is made within 17 hours of the current time. (3AM)
 today = datetime.now()
 
+print("Hours since last commit: ", today - latest_commit_time)
+
 if latest_commit_time is None or today - latest_commit_time > timedelta(hours=17):
     print("No commits found.")
     sys.exit(1)
